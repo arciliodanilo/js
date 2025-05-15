05760e334864ded8d608cdab91a482c9299c9ae4
arciliodanilo <arciliodanilo7@gmail.com>
2025-05-15 18:59:49 -0300

    Add files via upload

---
+++ aula3/aul.js
@@ -0,0 +1,26 @@
+ while (true) {
+     let saida = prompt("Se você quiser sai, digite sair ")
+     if (saida.toLowerCase() === "sair") {
+         break; // Sai do loop quando o usuário digita "sair"
+    }
+     let idadePai = parseFloat(prompt("Digite a idade do seu pai "));
+     let idadeUsuario = parseFloat(prompt("Digite sua idade "));
+     alert (idadePai-idadeUsuario);
+     alert("Seu pai foi pai ainda joven");
+     let idadeMae =parseFloat(prompt("Digite a idade do seu mãe"));
+     alert (idadeMae-idadeUsuario);
+     alert("Sua mãe foi mãe aindo joven")
+
+//      for (i = 0 ; i <= idadeUsuario && idadeUsuario < 18 ; i++) {
+//      console.log("Na primeira iteração:  "+ i + ", " +idadeUsuario+ " anos")
+//    }
+
+    for  (const idadeUsuario of menorDeIdade){
+    if(menorDeIdade<18) {
+      
+        alert("Você está proibido andar só!")
+        return menorDeIdade
+    }
+   }
+
+  }
